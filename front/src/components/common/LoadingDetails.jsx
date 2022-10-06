f0b9221399189650fc5bfd14f20be2a961ccc464
rolando-17 <rolandomrivas199@gmail.com>
2022-10-06 09:52:14 -0500

    Prueba tecnica front end ecomsur

---
+++ front/src/components/common/LoadingDetails.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+// import Skeleton from "react-loading-skeleton";
+
+const LoadingDetails = () => {
+  return (
+    <>
+      <div className="box-skeleton-6">sekeleton details</div>
+      <div className="box-skeleton-6">skeleton details</div>
+    </>
+  );
+};
+export default LoadingDetails;
